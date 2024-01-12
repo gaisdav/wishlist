f97d16da798fdc8c880f2642c5556ef9ad9b02ff
gaisdav <gais.davletkildin@gmail.com>
2024-01-12 16:39:21 +0100

    deleted useTheme.tsx hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useColorScheme } from '@mui/joy';
 import { PageWrapper } from './components/organisms/PageWrapper';
-import { RouterProvider } from 'react-router-dom';
+import { Link, RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes.tsx';
 import { BottomNavigation, Modal } from './components/molecules';
 import { Icon, IconButton } from './components/atoms';
-import { useThemeSwitcher } from './hooks/useTheme.tsx';
 import { useState } from 'react';
 
 function App() {
   const [open, setOpen] = useState(false);
-  const { changeTheme } = useThemeSwitcher();
+
+  const { mode, setMode } = useColorScheme();
+
+  const handleSwitchTheme = () => {
+    setMode(mode === 'light' ? 'dark' : 'light');
+  };
 
   const handleOpenModal = () => {
     setOpen(!open);
@@ -21,7 +26,7 @@ function App() {
       </PageWrapper>
 
       <BottomNavigation>
-        <IconButton onClick={changeTheme}>
+        <IconButton onClick={handleSwitchTheme} component={Link}>
           <Icon iconName="grid_view" />
         </IconButton>
 
@@ -29,10 +34,11 @@ function App() {
           <Icon iconName="add" />
         </IconButton>
 
-        <IconButton>
+        <IconButton component={Link}>
           <Icon iconName="account_circle" />
         </IconButton>
       </BottomNavigation>
+
       <Modal open={open} title="text" onClose={handleOpenModal}>
         content
       </Modal>
