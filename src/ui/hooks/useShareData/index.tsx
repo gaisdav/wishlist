bb8fffd7550d51e66e8587151b44dea3e42f6ab7
gaisdav <gais.davletkildin@gmail.com>
2024-08-18 15:00:39 +0200

    added error notifier

--- src/ui/hooks/useShareData/index.tsx
+++ src/ui/hooks/useShareData/index.tsx
@@ -1,40 +1,49 @@
 import { IUseShareData, TShareData } from './types.ts';
 import { useCallback, useState } from 'react';
+import { useStore } from 'hooks/useStore.tsx';
 
 const cache = new Map<string, Promise<File[]>>();
 
 export const useShareData = (data: TShareData): IUseShareData => {
+  const {
+    notification: { errorNotification },
+  } = useStore();
   const [loading, setLoading] = useState(false);
   const canShare = 'canShare' in navigator;
   const shareIcon = canShare ? 'share' : 'link';
 
   const share = useCallback(async () => {
     if (canShare) {
-      const { files: dataFiles } = data;
-      let files: File[] = [];
+      try {
+        const { files: dataFiles } = data;
+        let files: File[] = [];
 
-      if (typeof dataFiles === 'string') {
-        if (cache.has(dataFiles)) {
-          files = (await cache.get(dataFiles)) || [];
-        } else {
-          setLoading(true);
+        if (typeof dataFiles === 'string') {
+          if (cache.has(dataFiles)) {
+            files = (await cache.get(dataFiles)) || [];
+          } else {
+            setLoading(true);
 
-          try {
-            const response = await fetch(dataFiles);
-            const blob = await response.blob();
-            files = [
-              new File([blob], 'fileName', {
-                type: blob.type,
-              }),
-            ];
-          } catch (e) {
-            files = [];
-          } finally {
-            setLoading(false);
+            try {
+              const response = await fetch(dataFiles);
+              const blob = await response.blob();
+              files = [
+                new File([blob], 'fileName', {
+                  type: blob.type,
+                }),
+              ];
+            } catch (e) {
+              files = [];
+            } finally {
+              setLoading(false);
+            }
           }
         }
+        await navigator.share({ ...data, files });
+      } catch (error) {
+        await navigator.clipboard.writeText(data.url);
+        errorNotification(error);
       }
-      await navigator.share({ ...data, files });
     } else {
       await navigator.clipboard.writeText(data.url);
     }
